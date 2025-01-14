06d72870cfcd530b45d1601491313abfc671929c
jaeung <wodndrnjs@naver.com>
2025-01-14 22:35:09 +0900

    chore : google code style 적용

--- src/main/java/com/ict/esg/config/WebConfig.java
+++ src/main/java/com/ict/esg/config/WebConfig.java
@@ -5,10 +5,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    public void addCorsMappings(final CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET","POST");
-    }
+class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(final CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST");
+  }
 }
